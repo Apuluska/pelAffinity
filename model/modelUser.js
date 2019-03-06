12ec87bdaee51ad37fe72b94881cfafb7f2df006
Apuluska <46677502+Apuluska@users.noreply.github.com>
2019-03-06 16:52:53 +0100

    Update modelUser.js

--- model/modelUser.js
+++ model/modelUser.js
@@ -9,18 +9,6 @@ const userSchema = new Schema({
     birthDate: Date,
     password: { type: String, select: false },
 });
-/* userSchema.pre('save', (next) => {
-    let userSchema= user
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) return next(err)
-
-        bcrypt.hash(user.password, salt, null, (err, hash)=> {
-            if(err) return next(err)
-            
-            user.password = hash
-            next()
-        });
-    }); */
 });
 
 const userModel = mongoose.model("users", userSchema);
